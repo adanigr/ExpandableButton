474c00c4395a108e43a91b54e4214e4ceb8bf234
adanigr <adanigr@gmail.com>
2020-11-17 16:43:25 -0600

    Initial Commit

---
+++ ExpandableButton/ExpandableButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableButtonApp.swift
+//  ExpandableButton
+//
+//  Created by Adan Reséndiz on 17/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
